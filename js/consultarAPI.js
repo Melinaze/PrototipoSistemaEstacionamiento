8922fa973ed00535c84a331bfbbe440f19ec1f2e
Lucas Daniel Casella <lucascasel@gmail.com>
2021-06-09 10:28:19 -0300

    fix Index

--- js/consultarAPI.js
+++ js/consultarAPI.js
@@ -5,11 +5,11 @@ var tiposInfracciones_id = "tiposInfraccion/";
 var depositos_id = "depositos/";
 var acarreos_id = "/acarreos/"; // hay que poner el numero de infracción
 
-var requestInfracciones = function (patente) {
+var requestInfracciones = function(patente) {
     return $.ajax(url + patente + infracciones_id);
 };
 
-var responseExtract = function (attr, response) {
+var responseExtract = function(attr, response) {
     console.log(response);
     return response[attr];
 };
