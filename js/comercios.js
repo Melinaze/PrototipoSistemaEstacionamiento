a716eafb6772b77ab787184cb3009fec7ff8add7
Nacho2001 <nachosotelo9@gmail.com>
2021-06-14 15:25:53 -0300

    barra menu muestra los comericios con un btn

--- js/comercios.js
+++ js/comercios.js
@@ -38,18 +38,45 @@ var dibujarComercios = () => {
     map.addLayer(markerGroup);
 }
 
-/*var setComercioEnMenu = (comercio) => {
+var setComercioEnMenu = (comercio) => {
 
-    var li = comercio;
+   // var li = comercio;
+   var li = document.createElement('li');
 
-    li.id = nuevoLi;
 
-    li.innerHTML = "<span onclick='eliminar(this)'>X</span>" + nuevoLi;
+    //li.id = nuevoLi;
+   // console.log(comercio.getNombre())
+   li.innerHTML = comercio.getNombre();
+    
+   document.getElementById("menu").appendChild(li);    
+    
+    
+}
 
-    document.getElementById("menu").appendChild(li);
-}*/
 setComercios();
-dibujarComercios();
+
+
+
 
 
 // validacion de marker markerGroup._needsClustering[0]._popup._content == "El rey del pancho - pancheria"
+
+btnMostrarComercios.addEventListener("click", () => {
+    for (let i = 0; i < listaComercios.length; i++) {
+        setComercioEnMenu(listaComercios[i]);
+        dibujarComercios(); //habria q modificar un por el dibujar, o separarlo del drawer pq 
+        //dibuja dos veces el punto grande en rojo
+    }
+
+})
+
+function find_li(contenido) //no funca muy bien 
+        {
+            var el = document.getElementById("menu").getElementsByTagName("li");
+            for (var i=0; i<el.length; i++)
+            {
+                if(el[i].innerHTML==contenido)
+                    return false;
+            }
+            return true;
+        }
